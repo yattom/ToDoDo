bae2024161496dd7da749c80735f697f9154a622
yattom <tsutomu.yasui@gmail.com>
2021-11-09 15:28:34 +0900

    loaded files from Spring Initialzer

---
+++ src/main/java/jp/yattom/tododo/ToDoDoApplication.java
@@ -0,0 +1,13 @@
+package jp.yattom.tododo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/yattom/tododo/ToDoDoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.yattom.tododo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
