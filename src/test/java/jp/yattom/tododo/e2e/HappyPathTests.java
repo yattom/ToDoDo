a5d7e2a1fa35beacc245696936eba8d3b699a2fb
yattom <tsutomu.yasui@gmail.com>
2021-11-10 12:21:12 +0900

    add failing e2e test

---
+++ src/test/java/jp/yattom/tododo/e2e/HappyPathTests.java
@@ -0,0 +1,23 @@
+package jp.yattom.tododo.e2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HappyPathTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello, World");
+    }
+}
